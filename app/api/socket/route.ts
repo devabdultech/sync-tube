024ce97b30aa1c1a81037941cad92e4804fee6ac
AbdulTech <abdulhameedmoonstapha735@gmail.com>
2023-09-09 08:55:43 +0100

    ✔ replacing socket.io with pusher

--- app/api/socket/route.ts
+++ app/api/socket/route.ts
@@ -1,5 +1,4 @@
 import { NextResponse } from "next/server";
-import { Server, Socket } from "socket.io";
 
 export async function GET(request: Request) {
   return NextResponse.json("Hello World!");
