aac69a5ba8de5156c198e22a3c31af4a97190ae4
AbdulTech <abdulhameedmoonstapha735@gmail.com>
2023-10-17 23:44:13 +0100

    🎉 setting up

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ import { Inter } from "next/font/google";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-	title: "Sync Tube",
+	title: "Sync Tube - Watching Together, No Matter The Distance",
 	description: "Real-Time Synchronized Video Streaming and Chat Application"
 };
 
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,13 @@
 "use client";
 
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
 	return (
-		<main className="flex min-h-screen flex-col items-center justify-between">
+		<main className="flex min-h-[100dvh] flex-col items-center  bg-black text-white">
 			<h1>Sync Tube</h1>
 			<p>Real-Time Synchronized Video Streaming and Chat Application</p>
+			<Button>Get Started</Button>
 		</main>
 	);
 }
