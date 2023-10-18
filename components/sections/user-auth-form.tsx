44b12f59e83e180bed3a6bcdb81408da29fe0a40
AbdulTech <abdulhameedmoonstapha735@gmail.com>
2023-10-18 21:12:18 +0100

    🎉 finished user-auth-form component

--- components/sections/user-auth-form.tsx
+++ components/sections/user-auth-form.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import Image from "next/image";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 import * as z from "zod";
@@ -32,26 +33,41 @@ const UserAuthForm = () => {
 	}
 
 	return (
-		<Form {...form}>
-			<form onSubmit={form.handleSubmit(onSubmit)} className="w-full space-y-3">
-				<FormField
-					control={form.control}
-					name="email"
-					render={({ field }: { field: any }) => (
-						<FormItem>
-							<FormControl>
-								<Input placeholder="johndoe@example.com" {...field} />
-							</FormControl>
+		<div className="flex flex-col w-full space-y-4">
+			<Form {...form}>
+				<form onSubmit={form.handleSubmit(onSubmit)} className="w-full space-y-3">
+					<FormField
+						control={form.control}
+						name="email"
+						render={({ field }: { field: any }) => (
+							<FormItem>
+								<FormControl>
+									<Input placeholder="johndoe@example.com" {...field} />
+								</FormControl>
 
-							<FormMessage />
-						</FormItem>
-					)}
-				/>
-				<Button className="w-full" type="submit">
-					Sign In with Email
-				</Button>
-			</form>
-		</Form>
+								<FormMessage />
+							</FormItem>
+						)}
+					/>
+					<Button className="w-full" type="submit">
+						Sign In with Email
+					</Button>
+				</form>
+			</Form>
+
+			<div className="flex items-center justify-center">
+				<hr className="flex-grow border-gray-300" />
+				<h3 className="mx-4 text-muted-foreground text-sm uppercase">or continue with</h3>
+				<hr className="flex-grow border-gray-300" />
+			</div>
+
+			<Button variant={"outline"} className="w-full">
+				<div className="flex items-center justify-center gap-2">
+					<Image src="https://cdn.simpleicons.org/google" alt="Google Logo" width={15} height={15} className="" />
+					<span>Sign In with Google</span>
+				</div>
+			</Button>
+		</div>
 	);
 };
 
