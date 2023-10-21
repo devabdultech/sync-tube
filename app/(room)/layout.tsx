9dd906ccf62a0bdd26413335218cd5570e3d31c1
AbdulTech <abdulhameedmoonstapha735@gmail.com>
2023-10-21 08:01:37 +0100

    🔐 route protection - prod fix

--- app/(room)/layout.tsx
+++ app/(room)/layout.tsx
@@ -1,22 +1,3 @@
-import { cookies } from "next/headers";
-import { redirect } from "next/navigation";
-import {
-	createServerComponentClient
-} from "@supabase/auth-helpers-nextjs";
-
-
-export default async function RoomLayout({ children }: { children: React.ReactNode }) {
-	const supabase = createServerComponentClient({
-		cookies
-	});
-	
-	const {
-		data: { session }
-	} = await supabase.auth.getSession();
-
-	if (!session) {
-		redirect("/login");
-	}
-
+export default async function RoomLayout({ children }: { children: React.ReactNode }) {	
 	return <>{children}</>;
 }
