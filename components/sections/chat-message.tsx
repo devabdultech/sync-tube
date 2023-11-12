19ba0ddbbeab2554dd6cdba70f3443accc750223
AbdulTech <abdulhameedmoonstapha735@gmail.com>
2023-11-12 16:34:14 +0100

    ✨ added fallback avatar

--- components/sections/chat-message.tsx
+++ components/sections/chat-message.tsx
@@ -1,12 +1,21 @@
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { getRandomColor } from "@/lib/utils";
 
-const ChatMessage = ({ username }: { username: string }) => {
+const ChatMessage = ({ username, imageSrc }: { username: string; imageSrc?: string }) => {
 	return (
 		<div className="mb-3 flex gap-2">
-			<Avatar className="h-6 w-6">
-				<AvatarImage src="https://github.com/shadcn.png" />
-				<AvatarFallback>CN</AvatarFallback>
-			</Avatar>
+			{imageSrc ? (
+				<Avatar className="h-6 w-6">
+					<AvatarImage src={imageSrc} />
+				</Avatar>
+			) : (
+				<div
+					className={`bg-[${getRandomColor()}] flex h-6 w-6 items-center justify-center rounded-full border bg-red-300 p-3 text-center`}
+				>
+					<h4 className="text-center text-xs">{username.substring(0, 2).toUpperCase()}</h4>
+				</div>
+			)}
+
 			<div>
 				<h3 className="text-muted-foreground">{username}</h3>
 				<p className="text-sm leading-relaxed tracking-wide">
--- lib/utils.ts
+++ lib/utils.ts
@@ -1,12 +1,24 @@
 import { type ClassValue, clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
 
+// Function to merge Tailwind classes
 export function cn(...inputs: ClassValue[]) {
 	return twMerge(clsx(inputs));
 }
 
+// Function to extract date from timestamp
 export function extractDateFromTimestamp(timestamp: string) {
 	const createdAtDate = new Date(timestamp);
 	const dateStr = createdAtDate.toISOString().split("T")[0];
 	return dateStr;
+}
+
+// Function to generate a random color
+export function getRandomColor() {
+	const letters = "0123456789ABCDEF";
+	let color = "#";
+	for (let i = 0; i < 6; i++) {
+		color += letters[Math.floor(Math.random() * 16)];
+	}
+	return color;
 }
