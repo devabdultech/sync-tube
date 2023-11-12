449d3d0dde64eda44d8278f251ff74fb91f1984c
AbdulTech <abdulhameedmoonstapha735@gmail.com>
2023-11-12 12:49:36 +0100

    📝 added copy link and id feature for rooms

--- components/sections/room.tsx
+++ components/sections/room.tsx
@@ -1,31 +1,62 @@
 import Link from "next/link";
+import { useCopyToClipboard } from "@/hooks/copy";
+import { useToast } from "@/components/ui/use-toast";
 import { MousePointerSquareIcon } from "lucide-react";
 import { Card, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
+import {
+	ContextMenu,
+	ContextMenuContent,
+	ContextMenuItem,
+	ContextMenuTrigger
+} from "@/components/ui/context-menu";
 
-const Room = ({
-	roomId,
-	roomName,
-	createdAt
-}: {
+interface Room {
 	roomId: string;
 	roomName: string;
 	createdAt: string;
-}) => {
-	return (
-		<Link href={`/room/${roomId}`}>
-			<Card className="flex h-[250px] w-full flex-col justify-between lg:w-[300px]">
-				<CardHeader>
-					<CardTitle className="flex items-center text-lg">
-						<MousePointerSquareIcon className="mr-4 h-5 w-5" /> {roomName}{" "}
-					</CardTitle>
-				</CardHeader>
+}
+const Room = ({ roomId, roomName, createdAt }: Room) => {
+	const { toast } = useToast();
+	const [value, copy] = useCopyToClipboard();
 
-				<CardFooter className="flex flex-col items-start gap-1 text-muted-foreground">
-					<p>Created: {createdAt}</p>
-					<p>Members: 10</p>
-				</CardFooter>
-			</Card>
-		</Link>
+	const handleCopyRoomId = () => {
+		copy(roomId);
+		toast({
+			title: "Copied",
+			description: "Successfully Copied Room ID"
+		});
+	};
+
+	const handleCopyRoomLink = () => {
+		copy(`${window.location.origin}/room/${roomId}`);
+		toast({
+			title: "Copied",
+			description: "Successfully Copied Room Link"
+		});
+	};
+	return (
+		<ContextMenu>
+			<ContextMenuTrigger>
+				<Link href={`/room/${roomId}`}>
+					<Card className="flex h-[250px] w-full flex-col justify-between lg:w-[300px]">
+						<CardHeader>
+							<CardTitle className="flex items-center text-lg">
+								<MousePointerSquareIcon className="mr-4 h-5 w-5" /> {roomName}{" "}
+							</CardTitle>
+						</CardHeader>
+
+						<CardFooter className="flex flex-col items-start gap-1 text-muted-foreground">
+							<p>Created: {createdAt}</p>
+							<p>Members: 10</p>
+						</CardFooter>
+					</Card>
+				</Link>
+			</ContextMenuTrigger>
+			<ContextMenuContent>
+				<ContextMenuItem onClick={() => handleCopyRoomId()}>Copy Room ID</ContextMenuItem>
+				<ContextMenuItem onClick={() => handleCopyRoomLink()}>Copy Room Link</ContextMenuItem>
+			</ContextMenuContent>
+		</ContextMenu>
 	);
 };
 
